009e24dbcb204d2612adfefa538570739f53f6fb
sshnobi <mungaihpk@outlook.com>
2023-02-16 05:39:31 -0800

    Added c file to print statement.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
