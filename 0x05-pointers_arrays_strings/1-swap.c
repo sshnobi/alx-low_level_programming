a9a2323c50e0e790eb544dc1d5a437c9dfe3b078
Peter Mungai <124062272+sshnobi@users.noreply.github.com>
2023-02-28 17:07:57 +0300

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * swap_int - swap the value of two integers via pointers
+ * @a: pointer to integer value of first variable
+ * @b: pointer to integer value of second variable
+ */
+void swap_int(int *a, int *b)
+{
+	int c = &b;
+	*b = &a;
+	*a = c;
+}
