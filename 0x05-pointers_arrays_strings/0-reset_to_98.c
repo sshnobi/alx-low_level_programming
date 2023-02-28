fa016e3b33835959f004d9ac8ddc4e2cc576081f
Peter Mungai <124062272+sshnobi@users.noreply.github.com>
2023-02-28 16:28:18 +0300

    Create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * reset_to_98 - set a number to 98 via pointer
+ * @n: pointer to an integer variable n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
