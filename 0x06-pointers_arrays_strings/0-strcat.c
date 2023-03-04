0cdda185f599dc7f682aaa254e54dfe38678d028
Peter Mungai <124062272+sshnobi@users.noreply.github.com>
2023-03-04 13:39:22 +0300

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be appended
+ * @src: string to append to dest
+ * Return: dest - concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
