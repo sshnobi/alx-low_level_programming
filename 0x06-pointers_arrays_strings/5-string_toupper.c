246479c37af3da66cbc95fdd182c51d8ca84cd56
Peter Mungai <124062272+sshnobi@users.noreply.github.com>
2023-03-04 14:03:24 +0300

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - function to capitalize all letters in a string
+ * @s: string to capitalize
+ * Return: the capitalized string
+ */
+
+char *string_toupper(char *s)
+{
+
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
+	}
+	return (s);
+}
