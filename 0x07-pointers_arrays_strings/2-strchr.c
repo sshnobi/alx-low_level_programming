64cb54daea3b52b4b9dd49e1a9b94ddc505bc029
Peter Mungai <124062272+sshnobi@users.noreply.github.com>
2023-03-07 06:18:52 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
  * _strchr - a function to locate a character in a string
  * @s: the string to check
  * @c: the character to search
- * Return: a pointer to the first occurance of the character in the string. 
+ * Return: a pointer to the first occurance of the character in the string.
  * Return NULL if the character isn't found
  */
 
