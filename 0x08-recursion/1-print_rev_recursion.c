8151ff4d7961bd26e7aaabd1c04b84027f4b4574
Peter Mungai <124062272+sshnobi@users.noreply.github.com>
2023-03-10 05:53:07 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,9 +9,8 @@
 
 void _print_rev_recursion(char *s)
 {
-	while (*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
-		putchar(*s);
-	}
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	putchar(*s);
 }
