393e9f7551e91f94cd0f996e646e875724c067f0
Peter Mungai <124062272+sshnobi@users.noreply.github.com>
2023-03-10 05:55:35 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,16 @@
 
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: the string to check the length
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
