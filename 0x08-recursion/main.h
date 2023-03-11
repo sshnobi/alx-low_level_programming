0018bf94306270a5f453e1a54807a4148ac50d7d
Peter Mungai <124062272+sshnobi@users.noreply.github.com>
2023-03-11 12:34:26 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,6 +15,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int palindromeChecker(char *str, int len, int i);
+int _strlen_recursion(char *s);
 int wildcmp(char *s1, char *s2);
 
 #endif
